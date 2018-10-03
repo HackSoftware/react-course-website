d178badaf83554af8b255998b5c8d7d32d2e4c62
Ivaylo Bachvarof <bachvarof@gmail.com>
2018-10-03 11:32:33 +0300

    Add responsive Container component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Container from '../components/container'
 
 const IndexPage = () => (
-  <React.Fragment>
+  <Container>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-  </React.Fragment>
+  </Container>
 )
 
 export default IndexPage
