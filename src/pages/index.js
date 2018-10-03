7cc6f0cbce7d2af0c237440d8924b8a127f0fc1f
Ivaylo Bachvarof <bachvarof@gmail.com>
2018-10-03 11:50:23 +0300

    Add react-helment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {Helmet} from "react-helmet";
+
 import Container from '../components/container'
 
+
 const IndexPage = () => (
-  <Container>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Container>
+  <React.Fragment>
+    <Helmet>
+      <title>React Course</title>
+    </Helmet>
+
+    <Container>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Container>
+  </React.Fragment>
 )
 
 export default IndexPage
